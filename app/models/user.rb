8273c539cc8d38f5d1ac333c980d4134c906c826
Rabia <rabii.awajid@gmail.com>
2026-01-05 15:20:15 +0500

    Add Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord 
-
+validates :username, presence: true ,    uniqueness: { case_sensitive: false },  length: {minimum: 6 , maximum: 100}
+Valid_Email_Formate = \A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z
+    validates :email, presence: true ,    uniqueness: { case_sensitive: false },  length: {minimum: 6 , maximum: 100}, format: {with: Valid_Email_Formate}
 
 end 
