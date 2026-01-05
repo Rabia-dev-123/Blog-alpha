1489c6d65cab6efb2070abf8269013d3b9303223
Rabia <rabii.awajid@gmail.com>
2026-01-05 15:57:51 +0500

    Association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(params.require(:article).permit(:title , :description))
+    @article.user = User.first
     if @article.save()
 flash[:notice] = "Article saved sucessfully"
 redirect_to @article
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord 
+  has_many :articles
 validates :username, presence: true ,    uniqueness: { case_sensitive: false },  length: {minimum: 6 , maximum: 100}
-Valid_Email_Formate = \A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z
-    validates :email, presence: true ,    uniqueness: { case_sensitive: false },  length: {minimum: 6 , maximum: 100}, format: {with: Valid_Email_Formate}
+VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true ,    uniqueness: { case_sensitive: false },  length: {minimum: 6 , maximum: 100}, format: {with: VALID_EMAIL_FORMAT}
 
 end 
